69806e3011ebc3e997cb7ff817a6b161178218b0
Brentt Hawkins <brentt.hawkins@gmail.com>
2024-10-11 17:41:00 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+  // This is my first JavaScript code! 
+  console.log('Hello World');
+ const javascript= "javascript is fun!" 
+console.log(javascript) 
+let Brentt = 'Brentt Hawkins'
+console.log(Brentt);
+const x = 7 * 8
+const animals = ['lion', 'tiger', 'bear', 'zebra']
+
+Brentt = "Brentt Hawkins"
+console.log(Brentt);
+
+console.log(x, 'THIS IS X');
+ 
+console.log(animals[0], 'not Zerba here');
+
+
+if (animals[0] === 'lion') { console.log('Hello');
+
+    
+} else {
+    console.log('Goodbye');
+    }
